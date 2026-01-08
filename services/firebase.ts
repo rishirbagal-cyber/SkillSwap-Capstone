a7e5078e3614bf247b13b3e3247fc390382fc40b
Rishikesh Bagal <rishirbagal@gmail.com>
2026-01-08 17:07:40 +0530

    Final:Almost working app without AI Curated Roadmap.

--- services/firebase.ts
+++ services/firebase.ts
@@ -3,12 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "REMOVED_API_KEY",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "skillswap-g.firebaseapp.com",
   projectId: "skillswap-g",
   storageBucket: "skillswap-g.firebasestorage.app",
   messagingSenderId: "221972158406",
   appId: "1:221972158406:web:d999c58ea21ae357fcc4c7",
+  measurementId: "G-VGBD7ZPT3W"
 };
 
 const app = initializeApp(firebaseConfig);
