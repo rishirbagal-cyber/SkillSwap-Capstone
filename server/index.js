69de1b1ff6ce401bd3465b36ebae35c4d3f9e5e3
Rishikesh Bagal <rishirbagal@gmail.com>
2026-01-15 10:22:17 +0530

    Absolute CORS fix

--- server/index.js
+++ server/index.js
@@ -16,12 +16,17 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://skillswap-grow.netlify.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
   if (req.method === "OPTIONS") {
-    res.sendStatus(204);
-  } else {
-    next();
+    return res.sendStatus(200);
   }
+
+  next();
 });
+
 
 app.use(express.json());
 
